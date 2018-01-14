c4272e3699b307cea050391e226cd4e16ed610b5
akashcastelino <32956844+akashcastelino@users.noreply.github.com>
2018-01-15 00:11:06 +0100

    Dragon functionally implemented
    David,
    The StartDragonGameplay script has the StartDragonGameplayFunction() which you must call to begin the Dragon Tickling Sequence. 
    At the moment as placeholder, i have put a press Space key to enable the Dragon Tickling Sequence. You can omit that.

---
+++ MixedReality4_Adventure/FromBehindTheDragon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FromBehindTheDragon : MonoBehaviour {
+
+	public GameObject ARcam;
+	public GameObject Dragon;
+	//Dont forget to change these to private
+	public Vector3 ARCamDirection;
+	public Vector3 DragonDirection;
+	public static bool youAreBehind;
+
+	void Update()
+	{    youAreBehind = false;
+		ARCamDirection = ARcam.transform.forward;
+		DragonDirection = Dragon.transform.forward;
+
+		if (Vector3.Dot (ARCamDirection, DragonDirection) > 0) {
+			youAreBehind = true;
+			Debug.Log ("you are behind !!");
+		} else {
+			Debug.Log("you are in front !!");
+		}
+	}
+}
---
+++ MixedReality4_Adventure/StartDragonGamePlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDragonGamePlay : MonoBehaviour {
+	private TickleManager startScript;
+	// Use this for initialization
+	void Start () {
+		startScript = this.GetComponent<TickleManager> ();
+
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			StartDragonGamePlayFunction ();
+		}
+	}
+	
+	public void StartDragonGamePlayFunction()
+	{
+		startScript.enabled = true;
+	}
+}
